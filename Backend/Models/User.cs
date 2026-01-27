07bbab8a423a35fcf31e380b431af366a2cba750
Jonas Nielsen <jeksn@outlook.dk>
2026-01-27 08:22:55 +0100

    Added salt 🧂

--- Backend/Models/User.cs
+++ Backend/Models/User.cs
@@ -5,6 +5,7 @@
         public required string Name { get; set; }
         public required string Email { get; set; }
         public required string Password { get; set; }
+        public required string Salt { get; set; }
         public required string RealPassword { get; set; }
     }
 }

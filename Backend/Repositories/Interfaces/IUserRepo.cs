407e79b0820e55528398de472466c10fbd4a7d8c
Yevheniia Zhygalova <YevaITUA@gmail.com>
2026-01-30 09:18:30 +0100

    delete extra AddUser

--- Backend/Repositories/Interfaces/IUserRepo.cs
+++ Backend/Repositories/Interfaces/IUserRepo.cs
@@ -4,8 +4,6 @@ using System.Linq.Expressions;
 
 public interface IUserRepo
 {
-    public Task<User> GetUser(string id);
-
     User? GetByEmail(string email);
     void Add(User user);
     /// <summary>

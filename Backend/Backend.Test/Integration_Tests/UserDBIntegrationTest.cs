d34053c4013d94f9c0120a9f107aa1de58da928a
jhm20P <JMikkelsen40@outlook.com>
2026-02-03 20:02:46 +0100

    added some stuff

--- Backend/Backend.Test/Integration_Tests/UserDBIntegrationTest.cs
+++ Backend/Backend.Test/Integration_Tests/UserDBIntegrationTest.cs
@@ -1,6 +1,99 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Testcontainers.PostgreSql;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Models;
+using Repositories.Context;
+
 namespace Backend.Test.Integration_Tests;
 
 public class UserDBIntegrationTest
 {
+    private readonly PostgreSqlContainer postgressContainer = new PostgreSqlBuilder().Build();
+    private string _connectionString = string.Empty;
+    private WebApplicationFactory<Program> _factory;
+    public AppDBContext db { get; private set; } = default!;
+
+    [OneTimeSetUp]
+    public async Task OneTimeSetup()
+    {
+        await postgressContainer.StartAsync();
+        _connectionString = postgressContainer.GetConnectionString();
+        _factory = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    services.Remove(services.Single(a => a.ServiceType == typeof(DbContextOptions<AppDBContext>)));
+                    services.AddDbContext<AppDBContext>(options =>
+                    {
+                        options.UseNpgsql(_connectionString);
+                    });
+                });
+            });
+
+        db = _factory.Services.CreateScope().ServiceProvider.GetRequiredService<AppDBContext>();
+        await db.Database.MigrateAsync();
+    }
     
+
+
+    [Test]
+    public async Task Create_User_And_Check_If_It_Exists()
+    {
+
+        Console.WriteLine(_connectionString);
+        var user = new User()
+        {
+            Id = "1",
+            Name = "User",
+            Email = "user@test.com",
+            RealPassword = "password123",
+            HashedPassword = BCrypt.Net.BCrypt.HashPassword("password123"),
+            Salt = "BCrypt internal",
+            Base64Pfp = "profile_picture.png",
+            CreatedAt =  DateTime.UtcNow,
+            UpdatedAt =  DateTime.UtcNow,
+            
+        };
+        
+        db.Users.Add(user);
+        
+        await db.SaveChangesAsync();
+        
+        var foundUser = await db.Users.FindAsync(user.Id);
+        
+        Assert.That(foundUser, Is.Not.Null);
+        Assert.That(foundUser.Name, Is.EqualTo("User"));
+        Assert.That(foundUser.Name, Is.Not.EqualTo("User1"));
+        Assert.That(foundUser.Name, Is.Not.TypeOf<int>());
+        Assert.That(foundUser.Name, Is.TypeOf<string>());
+        Assert.That(foundUser.Name, Is.Not.TypeOf<bool>());
+        
+        Assert.That(foundUser.Id, Is.EqualTo("1"));
+        Assert.That(foundUser.Id, Is.Not.EqualTo("2"));
+        Assert.That(foundUser.Id, Is.Not.TypeOf<int>());
+        Assert.That(foundUser.Id, Is.TypeOf<string>());
+        Assert.That(foundUser.Id, Is.Not.TypeOf<bool>());
+        
+        
+        
+    }
+    
+    
+    
+    [OneTimeTearDown]
+    public async Task DisposeAsync()
+    {
+        await postgressContainer.DisposeAsync();
+        await _factory.DisposeAsync();
+        if (db != null)
+        {
+            await db.DisposeAsync();
+        }
+
+    }
 }
--- Backend/Backend.Test/Unit_Tests/Services/UserServiceTest.cs
+++ Backend/Backend.Test/Unit_Tests/Services/UserServiceTest.cs
@@ -1,6 +1,65 @@
+using Microsoft.EntityFrameworkCore;
+using Models;
+using Repositories;
+using Repositories.Context;
+using Services;
+
 namespace Backend.Test.Unit_Tests.Services;
 
 public class UserServiceTest
 {
-    
+    [SetUp]
+    public void Setup() {}
+
+    [Test]
+    public async Task Get_All_Users()
+    {
+            var options = new DbContextOptionsBuilder<AppDBContext>()
+                .UseInMemoryDatabase("ServiceUnitDB")
+                .Options;
+        
+            var db = new AppDBContext(options);
+        
+            var users = new List<User>();
+            for (int i = 0; i < 10; i++)
+            {
+                users.Add(new User
+                {
+                    Id = $"{i}",
+                    Name = $"user{i}",
+                    Base64Pfp = $"profile_pic_{i}",
+                    Email =  $"user{i}@email.com",
+                    HashedPassword = BCrypt.Net.BCrypt.HashPassword($"Password{i}"),
+                    Salt = "BCrypt internal",
+                    RealPassword = $"Password{i}",
+                    CreatedAt =  DateTime.Now,
+                    UpdatedAt =  DateTime.Now,
+                
+                });
+            }
+        
+            db.Users.Add(new User
+            {
+                Id = "1",
+                Name = $"user1",
+                Base64Pfp = $"profile_pic_1",
+                Email =  $"user1@email.com",
+                HashedPassword = BCrypt.Net.BCrypt.HashPassword($"Password1"),
+                Salt = "BCrypt internal",
+                RealPassword = $"Password1",
+                CreatedAt =  DateTime.Now,
+                UpdatedAt =  DateTime.Now,
+                
+            });
+        
+            await db.SaveChangesAsync();
+
+            var userRepo = new UserRepo(db);
+            var userService = new UserService(userRepo);
+            var user = await userService.GetUser("1");
+            
+            Assert.That(user.Id, Is.EqualTo("1"));
+
+            
+    }
 }

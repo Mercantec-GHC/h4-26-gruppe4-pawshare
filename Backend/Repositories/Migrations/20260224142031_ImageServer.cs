2c364b70f2503081270aadc637fc42eeb50f58e0
Yevheniia Zhygalova <eva.lypak@gmail.com>
2026-02-24 15:28:38 +0100

    add migrations

---
+++ Backend/Repositories/Migrations/20260224142031_ImageServer.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImageServer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Base64Pfp",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Base64Image",
+                table: "Animals",
+                newName: "AnimalPictureKey");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePictureKey",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePictureKey",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "AnimalPictureKey",
+                table: "Animals",
+                newName: "Base64Image");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Base64Pfp",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}

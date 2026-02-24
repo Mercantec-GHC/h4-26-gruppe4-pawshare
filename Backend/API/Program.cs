7019e7336b86de0aaab159766700b074490914ff
jhm20P <JMikkelsen40@outlook.com>
2026-02-24 09:28:42 +0100

    updated the pipeline

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -146,11 +146,7 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
- using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.    GetRequiredService<AppDBContext>();
-    dbContext.Database.Migrate();
-} 
+
 
 
 
